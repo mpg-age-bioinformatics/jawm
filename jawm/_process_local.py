8670bcd2f55e296ad7eafba6592072d5f411798a
sourovamin <sourov.amin@gmail.com>
2025-10-24 15:20:07 +0000

    better logging local

--- jawm/_process_local.py
+++ jawm/_process_local.py
@@ -148,8 +148,8 @@ def _execute_local(self):
                     self.execution_end_at = datetime.now().strftime('%Y%m%d_%H%M%S')
                     self.finished_event.set()
                     self.stop_future_event.set()
-                    ter_err = f"Process {self.name} failed with error:\n\n{error_message}"
-                    self.logger.error(f"{ter_err}\n{self._elog_path()}")
+                    self._log_error_summary(f"Process in Local failed.{self._tail_error()}", type_text="LocalAttempt")
+                    self.logger.error(f"Process {self.name} in Local failed after {total_attempts} attempts{self._elog_path()}{self._tail_error()}")
                     return
 
         # Start the background thread so _execute_local() returns immediately
