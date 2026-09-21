cc98b9998fcb4132a0a3182b8f92e04ca4db7eda
sourovamin <sourov.amin@gmail.com>
2026-09-21 14:32:37 +0200

    Prepare jawm 0.1.1 release

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 # Bridge to pyproject.toml for old tools
 from setuptools import setup
-setup(
-    name="jawm",
-    version="0.1.0",
-    extras_require={
-        "full": [
-            "pandas>=1.1",
-            "openpyxl>=3.0",
-            "requests"
-        ]
-    },
-)
+
+setup()
